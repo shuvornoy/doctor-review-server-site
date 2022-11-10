a1bd0cab10e73612200cb81c64be2a864abfacdb
shuvornoy <shuvornoy@gmaol.com>
2022-11-10 02:01:48 -0800

    vercel setup

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ async function run() {
 
         app.post('/jwt', (req, res) =>{
             const user = req.body;
-            const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '15d'})
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '5d'})
             res.send({token})
         }) 
 
